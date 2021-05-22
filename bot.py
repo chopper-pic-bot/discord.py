1435816673a40c5f422fadf51d6509f7f8c2b19d
trashalden <84301332+trashalden@users.noreply.github.com>
2021-05-22 14:47:25 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,42 @@
 import discord
-import os
 import time
-client=discord.Client()
-count=0
+import praw
+import os
 import datetime
 import aiohttp
-x = datetime.datetime.now()
-print(x)
+
+reddit=praw.reddit(
+  client_id=os.getenv('client_id'),
+  client_secret=os.getenv('client_secret'),
+  username=os.getenv('username'),
+  password=os.getenv('password'),
+  user_agent="<Chopperbot1.0>"
+)
+
+client=discord.client()
+count=0
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+  subreddit=reddit.subreddit("tonitonychopper")
+  all_subs=[]
+  top=subreddit.top(limit=1000)
+  for submission in top:
+    all_subs.append(submission)
+  random_sub=random.choice(all_subs)
+  url=random_sub.url
   global count
   while True:
     count=count+1
     embedtitle='Daily Chopper pic #'+str(count)+' :heart:'
-    channel = client.get_channel(842017214399119420)
+    channel=client.get_channel(842017214399119420)
     embedVar = discord.Embed(title=embedtitle, description=":sparkles: Daily pictures of Tony Tony Chopper",url="https://myanimelist.net/character/309/", color=0x54ffe5,timestamp=datetime.datetime.utcnow())
+    #embedVar.add_image(url=
     embedVar.add_field(name="Anime source", value="One piece", inline=True)
     embedVar.add_field(name="Retrieved from", value="imgscrapper", inline=True)
+    embedVar.add_image(url=url)
     embedVar.set_footer(text='\u200b',icon_url="https://i.imgur.com/uZIlRnK.png")
     await channel.send(embed=embedVar)
     time.sleep(86400) # Delay for 1 day
-
-
 
 @client.event
 async def on_member_join(member):
@@ -31,3 +45,5 @@ async def on_member_join(member):
 
 
 client.run(os.getenv('TOKEN'))
+
+
